b3e188e6cdf16ef6e535dfa6dbf24c7aee9df599
Elide Zavala <zavalavinagreelide@gmail.com>
2023-01-28 14:28:24 -0600

    routers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibroListaComponent } from './libro-lista/libro-lista.component';
+import { AutorListaComponent } from './autor-lista/autor-lista.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { 
+    path: 'libros',
+    component: LibroListaComponent
+  },
+  {
+    path: 'autores',
+    component: AutorListaComponent
+  },
+  {
+    path: '',
+    redirectTo: '/libros',
+    pathMatch: 'full' // <-- this is the default if the url is empty
+  },
+  {
+    path: '**', // <-- this is the default if the url is not found
+    component: NotFoundComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibroListaComponent,    AutorListaComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mocks.ts
@@ -0,0 +1,19 @@
+import { Libro } from './libro.model';
+
+export const LIBROS: Libro[] = [
+  {
+    "id": 1,
+    "titulo": "El Quijote",
+    "autor": "Cervantes"
+  },
+  {
+    "id": 2,
+    "titulo": "El Lazarillo de Tormes",
+    "autor": "Anónimo"
+  },
+  {
+    "id": 3,
+    "titulo": "El Príncipe",
+    "autor": "Maquiavelo"
+  },
+]
